5767c944b6d46fb2f3a7ae1883cf93228bf39d99
wu <wu.yuyuan@hotmail.com>
2020-06-24 00:49:08 +0200

    button at home page 'nous contacter'

--- view/homeView.php
+++ view/homeView.php
@@ -13,7 +13,9 @@
             </div>
             <div class="row btn-container">
                 <div class="col-md-6"><a href="index.php?action=login" class="btn btn-block bg-red">Connexion</a></div>
+                <div class="col-md-6"><a href="index.php?action=signup" class="btn btn-block bg-success">Nous contacter</a></div>
                 <div class="col-md-6"><a href="index.php?action=signup" class="btn btn-block bg-blue">Inscription</a></div>
+
             </div>
         </div>
     </div>
